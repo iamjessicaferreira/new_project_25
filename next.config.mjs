ca5b39d6fe24e016d8c321a69d43db73f70a1c97
mechamoje <devjessica.f@gmail.com>
2025-03-27 10:02:42 -0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 import mdx from "@next/mdx";
 
 const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {},
+	extension: /\.mdx?$/,
+	options: {},
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
+	pageExtensions: ["ts", "tsx", "md", "mdx"],
+	output: "export",
 };
 
 export default withMDX(nextConfig);
